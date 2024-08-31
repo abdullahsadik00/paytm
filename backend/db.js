95b97834bb92f0d713d7e68f7e8a2a1d2015c9a5
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-09-01 00:51:04 +0530

    Account Schema added

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,53 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 mongoose.connect(
-    'mongodb+srv://sadik:Sadik%403012@cluster0.7v7sara.mongodb.net/paytm'
-  );
-const {Schema} = mongoose;  
+  'mongodb+srv://sadik:Sadik%403012@cluster0.7v7sara.mongodb.net/paytm'
+);
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
- usename:String,
- password:String,firstname:String,
- lastname:String
+  usename: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 10,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 10,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 10,
+  },
+});
 
-})
+const accountSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
 
-const User = mongoose.model('User',userSchema)
-
-module.exports={
-    User
-}
+const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Balance', accountSchema);
+module.exports = {
+  User,
+  Account,
+};
