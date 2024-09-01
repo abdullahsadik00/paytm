6b95b7845a83b7c0e97d815d4160b7de416d60fc
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-09-02 01:06:58 +0530

    Signup Api bug resolved

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const rootRouter = require("./routes/index");
-const accountRouter = require("./routes/account");
 
 const app = express();
 const PORT = 3000;
